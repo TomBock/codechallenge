2ddefb08d91bbb9180ac2f665bcd4ea03d3db0f0
Tom <bockhorn.tom@web.de>
2020-11-22 15:41:22 +0100

    Added docs

--- src/main/java/de/bockhorn/codechallenge/OrderController.java
+++ src/main/java/de/bockhorn/codechallenge/OrderController.java
@@ -7,20 +7,34 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Controller for all order related topics
+ */
 @RestController
 public class OrderController {
 
+    /**
+     * Maps the /post suburl to receive orders and
+     * store the respective models in the database.
+     * @param order Model of the order, which has been pre-mapped by Jackson ot the java class.
+     * @return Displayed result string.
+     */
     @PostMapping("/post")
-    public String post(@RequestBody Order order) {
+    public String post(@RequestBody OrderModel order) {
         return order.toString();
     }
 
+    /**
+     * Maps the /get suburl to present all order entries
+     * that are stored in the database.
+     * @return ResponseEntity containing of a list of orders and status.
+     */
     @GetMapping("/get")
-    public ResponseEntity<List<Order>> get() {
+    public ResponseEntity<List<OrderModel>> get() {
 
-        List<Order> orders = new ArrayList<>();
-        orders.add(new Order(1, "Produkt 1", 3));
-        orders.add(new Order(2, "Produkt 2", 5));
+        List<OrderModel> orders = new ArrayList<>();
+        orders.add(new OrderModel(1, "Produkt 1", 3));
+        orders.add(new OrderModel(2, "Produkt 2", 5));
 
         return new ResponseEntity<>(orders, HttpStatus.OK);
     }
--- src/main/java/de/bockhorn/codechallenge/OrderModel.java
+++ src/main/java/de/bockhorn/codechallenge/OrderModel.java
@@ -4,10 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * POJO Model class for Order with an ID, a product and the amount ordered.
+ * Will automatically be mapped to the json format by Jackson (see dependencies).
+ */
 @AllArgsConstructor
-public class Order {
+public class OrderModel {
 
     @Getter @Setter int orderID;
+    // The orderID is assumed to automatically be increased by VUE (frontend).
+
     @Getter @Setter private String product;
     @Getter @Setter private int amount;
 
