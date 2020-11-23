12c12ce8250fabf173a297d7fbb4bea2043ff13b
Tom <bockhorn.tom@web.de>
2020-11-23 15:33:25 +0100

    Implemented Order repository and database connection to postgresql.

--- src/main/java/de/bockhorn/codechallenge/controller/OrderController.java
+++ src/main/java/de/bockhorn/codechallenge/controller/OrderController.java
@@ -2,6 +2,8 @@ package de.bockhorn.codechallenge.controller;
 
 import de.bockhorn.codechallenge.model.Order;
 import de.bockhorn.codechallenge.model.Product;
+import de.bockhorn.codechallenge.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,15 +17,18 @@ import java.util.List;
 @RestController
 public class OrderController {
 
+    @Autowired
+    private OrderRepository orderRepository;
+
     /**
      * Maps the /post suburl to receive orders and
      * store the respective models in the database.
      * @param order Model of the order, which has been pre-mapped by Jackson ot the java class.
      * @return Displayed result string.
      */
-    @PostMapping("/post")
-    public String post(@RequestBody Order order) {
-        return order.toString();
+    @PostMapping(value = "/post")
+    public Order post(@RequestBody Order order) {
+        return orderRepository.save(order);
     }
 
     /**
@@ -32,8 +37,9 @@ public class OrderController {
      * @return ResponseEntity containing of a list of orders and status.
      */
     @GetMapping("/get")
-    public ResponseEntity<List<Order>> get() {
+    public List<Order> get() {
 
+        /*
         List<Order> orders = new ArrayList<>();
         List<Product> products = new ArrayList<>();
 
@@ -44,8 +50,8 @@ public class OrderController {
         products.add(product2);
 
         orders.add(new Order(1, products));
-
-        return new ResponseEntity<>(orders, HttpStatus.OK);
+        */
+        return orderRepository.findAll();
     }
 
 }
--- src/main/java/de/bockhorn/codechallenge/model/Order.java
+++ src/main/java/de/bockhorn/codechallenge/model/Order.java
@@ -2,21 +2,29 @@ package de.bockhorn.codechallenge.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.List;
 
 /**
  * POJO Model class for Order with an ID, a product and the amount ordered.
  * Will automatically be mapped to the json format by Jackson (see dependencies).
  */
+@Entity
+@Table(name = "orders")
+@NoArgsConstructor
 @AllArgsConstructor
 public class Order {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter @Setter private int orderID;
     // The orderID is assumed to automatically be increased by VUE (frontend).
-    @Getter @Setter int orderID;
 
-    @Getter @Setter private List<Product> products;
+    @Column(columnDefinition = "text")
+    @Getter @Setter private String products;
 
     @Override
     public String toString() {
